b9a4c8e0b387a1050d6bdc5d0ab4b0512eaea8bf
Shtill <shtilyao@gmail.com>
2026-07-05 12:38:19 +0300

    hw-js-8

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", event => {
-    const name = event.target.value.trim();
-
-    nameOutput.textContent = name || "Anonymous";
-});
